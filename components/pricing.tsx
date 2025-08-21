f397ba2d7006b3d5d547a1bffddbed3515eeffd6
Tanpreet <tanpreetjhally300@gmail.com>
2025-08-21 17:35:42 +0530

    Services

--- components/pricing.tsx
+++ components/pricing.tsx
@@ -43,28 +43,6 @@ export function Pricing({
     setIsMonthly(!checked);
     if (checked && switchRef.current) {
       const rect = switchRef.current.getBoundingClientRect();
-      const x = rect.left + rect.width / 2;
-      const y = rect.top + rect.height / 2;
-
-      // confetti({
-      //   particleCount: 50,
-      //   spread: 60,
-      //   origin: {
-      //     x: x / window.innerWidth,
-      //     y: y / window.innerHeight,
-      //   },
-      //   colors: [
-      //     "hsl(var(--primary))",
-      //     "hsl(var(--accent))",
-      //     "hsl(var(--secondary))",
-      //     "hsl(var(--muted))",
-      //   ],
-      //   ticks: 200,
-      //   gravity: 1.2,
-      //   decay: 0.94,
-      //   startVelocity: 30,
-      //   shapes: ["circle"],
-      // });
     }
   };
 
