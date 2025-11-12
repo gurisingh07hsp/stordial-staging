c0a39def1eecb3b6679f6ef6b2a927071bdd758f
Tanpreet <tanpreetjhally300@gmail.com>
2025-11-12 14:21:47 +0530

    meta data added

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,12 +5,16 @@ import ClientHeader from '../components/ClientHeader';
 import Footer from '../components/Footer';
 import AuthModal from '../components/AuthModal';
 import { Providers } from './providers';
+import Script from "next/script";
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Stordial - Find Local Businesses',
   description: 'Discover and connect with the best local businesses in your area. Find restaurants, hotels, services, and more.',
+  verification: {
+    google: "N61LeFw-PwRB43fxm7OGjrtbtUbA-XK3WS-Iw38gGq8",
+  },
 };
 
 export default function RootLayout({
@@ -21,6 +25,20 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+    <head>
+       <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-5D75P93DCH"
+        />
+        <Script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-5D75P93DCH');
+          `}
+        </Script>
+    </head>
       <body className={inter.className}>
         {/* <AuthProvider> */}
          <Providers>
